de622a88aee7dbad2ae22462e9034310a9932a4f
juanesosorio <Juanchis_1989@hotmail.com>
2021-05-16 22:58:59 -0500

    header mobile-desk listo

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="./src//assets//style/Style.css">
+    <link rel="stylesheet" href="./src/assets/style/header.css">
+    <link rel="stylesheet" href="./src/assets/style/style.css">
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Pagina de Inicio</title>
